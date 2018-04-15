b0b54464b8c44a638ce1ced9d96a5c2962ea8cf0
Chris Atkins <catkins@zendesk.com>
2018-04-15 19:57:14 +1000

    stub some usage of gopher-lua to get imports working

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/catkins/qbt/pkg/lua"
 	"github.com/spf13/cobra"
 )
 
@@ -19,10 +20,18 @@ func root() *cobra.Command {
 	rootCmd := cobra.Command{
 		Use:   "qbt",
 		Short: "qbt is a query interface for Google Cloud BigTable",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("hello qbt")
-		},
+		Run:   queryBigtable,
+		Args:  cobra.ExactArgs(1),
 	}
 
 	return &rootCmd
+}
+
+func queryBigtable(cmd *cobra.Command, args []string) {
+	env := lua.NewEnvironment()
+	source := args[0]
+	err := env.Eval(source)
+	if err != nil {
+		fmt.Printf("error running script: %v\n", err)
+	}
 }
